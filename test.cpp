e7f618f275b643c5b1ce0dffb007966c55eac5e6
mehmet0bastug <mehmet.bastug@sabanciuniv.edu>
2023-07-18 00:02:14 +0300

    changed test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
-
-TEST(Selamlardeneme , Deneme){
-    ASSERT_TRUE(true);
+static int square (int a){
+    return a*a;
+}
+TEST(FactorialDeneme , Deneme){
+    EXPECT_EQ(square(5),25);
 }
 int main(int argc , char **argv){
     testing::InitGoogleTest();
